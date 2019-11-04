640e4e795f678d1e3bf2b765e626135e0075a639
Goay Eng Yew <vinc3nt86@hotmail.com>
2019-11-04 10:11:11 +0800

    implement task 3; search book query

--- server/server.js
+++ server/server.js
@@ -17,9 +17,51 @@ app.use(morgan('tiny'));
 // Setup MySQL
 const pool = mysql.createPool(require('./config'));
 
-// Query
+// Queries
+const SEARCH_BOOK_BY_NAME_OR_TITLE =
+    'Select title, authors from book2018 where title like ? or authors like ? limit ? offset ?';
+const COUNT_BOOKS_SEARCH_BY_NAME_OR_TITLE =
+    'Select count(*) as book_count from book2018 where title like ? or authors like ?';
+const searchBooksByNameOrTitle = mkQuery(SEARCH_BOOK_BY_NAME_OR_TITLE, pool);
+
+
+const countBooksByNameOrTitle = mkQuery(COUNT_BOOKS_SEARCH_BY_NAME_OR_TITLE, pool);
 
 /// Define routes
+app.get('/api/search', (req, res) => {
+    const searchTerm = `%${req.query.terms || ''}%`;
+    const limit = req.query.size || 10;
+    const offset = req.query.start || 0;
+
+    const p0 = searchBooksByNameOrTitle([searchTerm, searchTerm, limit, offset]);
+    const p1 = countBooksByNameOrTitle([searchTerm, searchTerm]);
+
+    Promise.all([p0, p1])
+        .then(results => {
+            const data = results[0];
+            const count = results[1];
+
+            const bookResponse = {
+                data: data,
+                terms: searchTerm,
+                timestamp: new Date().getTime(),
+                total: count,
+                limit: limit,
+                offset: offset
+            }
+
+            console.log('searchBookByNameOrTitle Result: ', bookResponse);
+            console.log('CountBook Result: ', count);
+        })
+        .catch(error => {
+            const errorResponse = {
+                status: 400,
+                message: error,
+                timestamp: new Date().getTime()
+            }
+            console.log('ErrorResponse: ', errorResponse);
+        })
+})
 
 /// Start application
 app.listen(PORT, () => {
